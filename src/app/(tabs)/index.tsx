b2f2bb668d9ecd9a803fc86463c33958a08fb22c
Mirco Bisoffi <bismirco@gmail.com>
2026-05-08 15:31:11 +0200

    created (auth) route with signin and signout tsx

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,27 +1,34 @@
-import { Ionicons } from "@expo/vector-icons";
-import { Button } from "@react-navigation/elements";
 import { Image } from "expo-image";
 import { Link, useRouter } from "expo-router";
 import React from "react";
-
-
-import { ActivityIndicator, StyleSheet, Text, TextInput, View } from "react-native";
-
+import {
+    ActivityIndicator,
+    Button,
+    Platform,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+} from "react-native";
 export default function Index() {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <View style={styles.container}>
       <Text style={styles.helloWorldTitle}>CIAOOOO</Text>
-      <Image 
-        source={ {uri: "https://media.tenor.com/HHZOPnpoWPMAAAAM/why-you-lying.gif"}}
+      <Image
+        source={{
+          uri: "https://media.tenor.com/HHZOPnpoWPMAAAAM/why-you-lying.gif",
+        }}
         style={styles.image}
       />
       <TextInput placeholder="E-mail"></TextInput>
       <ActivityIndicator size={"large"}></ActivityIndicator>
-      <Link href={"/about"}>Go to about page</Link> 
-      <Button onPress={() => router.push("/scanner")}>
-        <Ionicons  name="add-circle-outline"/>
-      </Button>
+      <Link href={"/about"}>Go to about page</Link>
+      {Platform.OS === "android" ? (
+        <Button title="Scanner Andr" onPress={() => router.push("/scanner")} />
+      ) : (
+        <Button title="Scanner IOS" onPress={() => router.push("/scanner")} />
+      )}
     </View>
   );
 }
@@ -39,5 +46,5 @@ const styles = StyleSheet.create({
   image: {
     width: 200,
     height: 200,
-  }
+  },
 });
